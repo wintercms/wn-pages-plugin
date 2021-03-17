47c23ce1e9776064a935f1837be1885df678df20
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 11:21:44 -0400

    use new lazy Class Loader aliasing

--- aliases.php
+++ aliases.php
@@ -1,4 +1,7 @@
 <?php
+
+use Winter\Storm\Support\ClassLoader;
+
 /**
  * To allow compatibility with plugins that extend the original RainLab.Pages plugin, this will alias those classes to
  * use the new Winter.Pages classes.
@@ -25,8 +28,4 @@ $aliases = [
     Winter\Pages\Widgets\SnippetList::class          => 'RainLab\Pages\Widgets\SnippetList',
 ];
 
-foreach ($aliases as $original => $alias) {
-    if (!class_exists($alias)) {
-        class_alias($original, $alias);
-    }
-} 
+app(ClassLoader::class)->addAliases($aliases);
