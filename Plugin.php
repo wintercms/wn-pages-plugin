50b45e9fdbd2d4e1f94041290abcd70181a99943
Marc Jauvin <marc.jauvin@gmail.com>
2021-04-19 08:03:33 -0400

    replaces up to 1.3.7

--- Plugin.php
+++ Plugin.php
@@ -21,7 +21,7 @@ class Plugin extends PluginBase
             'author'      => 'Winter CMS',
             'icon'        => 'icon-files-o',
             'homepage'    => 'https://github.com/winter/pages-plugin',
-            'replaces'    => 'RainLab.Pages'
+            'replaces'    => ['RainLab.Pages' => '<= 1.3.7'],
         ];
     }
 
