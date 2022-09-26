f9cbaf0462a3e19501216082df5aa8d22c4b9385
Luke Towers <luke@luketowers.ca>
2022-09-26 00:43:48 -0600

    Use the already loaded staticPage object when present instead of re-resolving it

--- components/StaticPage.php
+++ components/StaticPage.php
@@ -79,8 +79,12 @@ class StaticPage extends ComponentBase
             return;
         }
 
-        $router = new Router(Theme::getActiveTheme());
-        $this->pageObject = $this->page['page'] = $router->findByUrl($url);
+        if (isset($this->controller->getPage()->apiBag['staticPage'])) {
+            $this->pageObject = $this->page['page'] = $this->controller->getPage()->apiBag['staticPage'];
+        } else {
+            $router = new Router(Theme::getActiveTheme());
+            $this->pageObject = $this->page['page'] = $router->findByUrl($url);
+        }
 
         if ($this->pageObject) {
             $this->title = $this->page['title'] = array_get($this->pageObject->viewBag, 'title');
