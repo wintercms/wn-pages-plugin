ff8a10b88d4470d9bca666d84c496cc37288b8e5
Marc Jauvin <marc.jauvin@gmail.com>
2023-02-15 12:48:27 -0500

    document why some code was commented out

--- classes/SnippetLoader.php
+++ classes/SnippetLoader.php
@@ -31,6 +31,7 @@ class SnippetLoader
 
         // Make an unique alias for this snippet based on its name and parameters
         #$snippetInfo['code'] = uniqid($snippetInfo['code'] . '_' . md5(serialize($snippetInfo['properties'])) . '_');
+        // the line above was commented out to allow the overriden partials in theme to be used for the component alias
 
         self::attachComponentSnippetToController($snippetInfo, $controller, true);
         self::cacheSnippet($snippetInfo['code'], $snippetInfo);
