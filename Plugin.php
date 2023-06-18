7db20dc64abc8a37e46f4ca221eb2fc40f39be37
Luke Towers <luke@luketowers.ca>
2023-06-17 23:34:03 -0600

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -146,7 +146,8 @@ class Plugin extends PluginBase
     {
         return [
             'filters' => [
-                'staticPage' => ['Winter\Pages\Classes\Page', 'url']
+                'staticPage'    => [StaticPage::class, 'url']
+                'parseSnippets' => [Snippet::class, 'parse'],
             ]
         ];
     }
@@ -389,58 +390,5 @@ class Plugin extends PluginBase
                 $widget->addJs('/plugins/winter/pages/assets/js/froala-snippets.js', 'Winter.Pages');
             });
         });
-    }
-
-    /**
-     * Register new Twig variables
-     */
-    public function registerMarkupTags(): array
-    {
-        return [
-            'filters' => [
-                'staticPage'    => ['Winter\Pages\Classes\Page', 'url'],
-                'parseSnippets' => ['Winter\Pages\Classes\Snippet', 'parse'],
-            ]
-        ];
-    }
-
-    public function registerClassAliases()
-    {
-        /**
-         * To allow compatibility with plugins that extend the original RainLab.Pages plugin,
-         * this will alias those classes to use the new Winter.Pages classes.
-         */
-        return [
-            \Winter\Pages\Plugin::class                       => \RainLab\Pages\Plugin::class,
-            \Winter\Pages\Classes\Content::class              => \RainLab\Pages\Classes\Content::class,
-            \Winter\Pages\Classes\Controller::class           => \RainLab\Pages\Classes\Controller::class,
-            \Winter\Pages\Classes\Menu::class                 => \RainLab\Pages\Classes\Menu::class,
-            \Winter\Pages\Classes\Page::class                 => \RainLab\Pages\Classes\Page::class,
-            \Winter\Pages\Classes\Router::class               => \RainLab\Pages\Classes\Router::class,
-            \Winter\Pages\Classes\Snippet::class              => \RainLab\Pages\Classes\Snippet::class,
-            \Winter\Pages\Classes\SnippetManager::class       => \RainLab\Pages\Classes\SnippetManager::class,
-            \Winter\Pages\Components\ChildPages::class        => \RainLab\Pages\Components\ChildPages::class,
-            \Winter\Pages\Components\StaticBreadcrumbs::class => \RainLab\Pages\Components\StaticBreadcrumbs::class,
-            \Winter\Pages\Components\StaticMenu::class        => \RainLab\Pages\Components\StaticMenu::class,
-            \Winter\Pages\Components\StaticPage::class        => \RainLab\Pages\Components\StaticPage::class,
-            \Winter\Pages\Controllers\Index::class            => \RainLab\Pages\Controllers\Index::class,
-            \Winter\Pages\FormWidgets\MenuItems::class        => \RainLab\Pages\FormWidgets\MenuItems::class,
-            \Winter\Pages\FormWidgets\MenuItemSearch::class   => \RainLab\Pages\FormWidgets\MenuItemSearch::class,
-            \Winter\Pages\FormWidgets\PagePicker::class       => \RainLab\Pages\FormWidgets\PagePicker::class,
-            \Winter\Pages\Widgets\MenuList::class             => \RainLab\Pages\Widgets\MenuList::class,
-            \Winter\Pages\Widgets\PageList::class             => \RainLab\Pages\Widgets\PageList::class,
-            \Winter\Pages\Widgets\SnippetList::class          => \RainLab\Pages\Widgets\SnippetList::class,
-        ];
-    }
-
-    public static function clearCache()
-    {
-        $theme = Theme::getEditTheme();
-
-        $router = new Router($theme);
-        $router->clearCache();
-
-        StaticPage::clearMenuCache($theme);
-        SnippetManager::clearCache($theme);
     }
 }
